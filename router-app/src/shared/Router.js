3438baa8e8c456c7c00de993ba289a62d32b8b1e
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-02-10 11:32:51 +0900

    Create Router.js router set-up

---
+++ router-app/src/shared/Router.js
@@ -0,0 +1,22 @@
+// router 설정 파일
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Works from "../pages/Works";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} /> 
+                <Route path="about" element={<About />}/>
+                <Route path="contact" element={<Contact />}/>
+                <Route path="works" element={<Works />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+// boilerplate와 밀접한 관계가 있다. 이해하지 말고 사용법 숙지!!
+
+export default Router;
