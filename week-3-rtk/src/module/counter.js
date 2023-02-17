4dcc12909fd97fd5a37664cda83df432d36410ca
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-02-17 10:48:33 +0900

    redux toolkit part1 - counter

--- week-3-rtk/src/module/counter.js
+++ week-3-rtk/src/module/counter.js
@@ -1,37 +1,56 @@
-const ADD_NUMBER = "ADD_NUMBER";
-const MINUS_NUMBER = "MINUS_NUMBER";
+import { createSlice } from "@reduxjs/toolkit";
 
-export const addNumber = (payload) => {
-    return {
-        type: ADD_NUMBER,
-        payload,
-    };
-};
+// action value
+// const ADD_NUMBER = "ADD_NUMBER";
+// const MINUS_NUMBER = "MINUS_NUMBER";
 
-export const minusNumber = (payload) => {
-    return {
-        type: MINUS_NUMBER,
-        payload,
-    };
-};
+// export const addNumber = (payload) => {
+//     return {
+//         type: ADD_NUMBER,
+//         payload,
+//     };
+// };
+
+// export const minusNumber = (payload) => {
+//     return {
+//         type: MINUS_NUMBER,
+//         payload,
+//     };
+// };
 
 const initialState = {
     number: 0,
 };
 
-const counter = (state = initialState, action) => {
-    switch (action.type){
-        case ADD_NUMBER:
-            return {
-                number: state.number + action.payload,
-            };
-        case MINUS_NUMBER:
-            return {
-                number: state.number - action.payload,
-            };
-        default:
-            return state;
-    }
-}
+// <Reducer>
+// const counter = (state = initialState, action) => {
+//     switch (action.type){
+//         case ADD_NUMBER:
+//             return {
+//                 number: state.number + action.payload,
+//             };
+//         case MINUS_NUMBER:
+//             return {
+//                 number: state.number - action.payload,
+//             };
+//         default:
+//             return state;
+//     }
+// }
 
-export default counter;
+// <redux toolkit>
+const counterSlice = createSlice({
+    name: "counter",
+    initialState,
+    reducers: {
+        addNumber: (state, action) => {
+            state.number = state.number + action.payload;
+        },
+        minusNumber: (state, action) => {
+            state.number = state.number - action.payload;
+        },
+    },
+});
+
+export default counterSlice.reducer;
+export const {addNumber, minusNumber} = counterSlice.actions;
