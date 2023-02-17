f840a4e5a9087d10fc2928d8393e4b21e1583f1d
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-02-17 20:50:40 +0900

    patch function

--- week-3-axios/src/App.jsx
+++ week-3-axios/src/App.jsx
@@ -6,43 +6,54 @@ function App() {
   const [inputValue, setInputValue] = useState({
     title:'',
   })
+  const [targetID, setTargetID] = useState('');
+  const [content, setContent] = useState('');
 
-  // get요청을 할 때 id가 들어온다.
+  // 조회 함수
   const fetchTodos = async() => {
     const {data} = await axios.get('http://localhost:4000/todos');
     console.log('data: ', data);
     setTodos(data);
   }
 
-  // 제출하면 발생하는 핸들러 : submit을 눌렀을 때 post요청
-  // 새로고침 안하고 갱신이 되었으면 좋겠다? -> state도 같이 렌더링 시켜줘야 한다. 컴포넌트(화면)도 렌더링 시켜줘야 한다.
-  // -> todos를 변경해줘야 한다.(setTodo)
+  // 추가 함수
   const onSubmitHandler = async () => {
     axios.post("http://localhost:4000/todos", inputValue);
     // fetchTodos();
     setTodos([...todos, inputValue]);
   }
 
+  // 삭제 함수
   const onDeleteBtnClickHandler = async (id) => {
     axios.delete(`http://localhost:4000/todos/${id}`);
     setTodos(todos.filter((item)=>item.id!==id));
   }
 
+  // 수정 함수
+  const onUpdateBtnClickHandler = async () => {
+    axios.patch(`http://localhost:4000/todos/${targetID}`, {
+      title: content,
+    })
+    setTodos(todos.map((item)=>item.id == targetID ? {...item, title:content} : item));
+  }
+
   useEffect(() => {
-    // db로부터 값을 가져올 것이다. 
     fetchTodos();
   }, [])
   return (
     <>
+    <div className="patch">
+      <input value={targetID} onChange={(e)=>setTargetID(e.target.value)} type="text" placeholder="수정할 아이디" />
+      <input value={content} onChange={(e)=>setContent(e.target.value)} type="text" placeholder="수정할 내용" />
+      <button onClick={onUpdateBtnClickHandler}> 수정 </button>
+    </div>
       <div className="input">
         <form onSubmit={(e)=>{
           e.preventDefault();
-
-          // 버튼 클릭 시, input에 들어있는 값(state)을 이용하여 DB에 저장(post 요청)
           onSubmitHandler();
         }}>
           <input type="text" value={inputValue.title} onChange={(e)=>setInputValue({title:e.target.value})} />
-          <button type="submit" >add</button>
+          <button type="submit" > 추가 </button>
         </form>
       </div>
       <div className="data">
