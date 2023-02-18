24b1d8c5137038a945a753fde946b07a66ddf1e6
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-02-18 20:45:49 +0900

    Button component

--- week-2-lv3/src/App.tsx
+++ week-2-lv3/src/App.tsx
@@ -1,11 +1,37 @@
-import './App.css';
+import styled from "styled-components";
+import Button from "./components/Button";
+import Input from "./components/Input";
+import Modal from "./components/Modal";
+import Select from "./components/Select";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <StContainer>
+      <StBox>
+        <h1>Button</h1>
+        <Button />
+      </StBox>
+      <StBox>
+        <h1>Input</h1>
+        <Input />
+      </StBox>
+      <StBox>
+        <h1>Modal</h1>
+        <Modal />
+      </StBox>
+      <StBox>
+        <h1>Select</h1>
+        <Select />
+      </StBox>
+    </StContainer>
   );
 }
 
 export default App;
+
+const StContainer = styled.div`
+  padding: 20px 30px;
+`;
+const StBox = styled.div`
+  
+`;
